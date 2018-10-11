451ac9e025f0990337db5c0e3381149bee8fe34f
GERMAIN ARTURO DURAN TORRES <arturodurantorres@gmail.com>
2018-10-11 03:32:56 +0000

    automatically backed up by learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -2,11 +2,13 @@
 def levitation_quiz
 	#your code here
 	loop do
-	answer=gets.chomp  
-	puts ""
-	break if answer ==
+	  puts "What is the spell that enacts levitation?"
+	  answer=gets.chomp  
+	
+	  break if answer == "Wingardium Leviosa"
 	end
-	puts ""
+	
+	puts "You passed the quiz!"
 end
 
 
